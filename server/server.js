12e42162f32d4d30851b168b5bc8661051d6e9d3
Shubham Kapoor <shubham.kapoor01@gmail.com>
2021-10-17 01:10:04 +0530

    commit on master

--- server/server.js
+++ server/server.js
@@ -15,18 +15,40 @@ const io = new Server(server, {
 	},
 })
 
+var users = [];
+function user(room, id, x, y) {
+	this.room = room;
+	this.id = id;
+	this.x = x;
+	this.y = y;	
+}
+
 io.on('connection', (socket) => {
 
 	socket.on('join-room', (data) => {
-		socket.join(data)
-		console.log(socket.id, ' has joined the room ', data)
+		var tempUser = new user(data.room, socket.id, 400, 100);
+		users.push(tempUser);
+		socket.join(data);
 	})
 
 	socket.on('send-message', (data) => {
 		socket.to(data.room).emit('receive-message', data)
 	})
 
+	socket.on('send-move', (data) => {
+		var user;
+		for (var i = 0; i < users.length; i++) {
+			if (socket.id === users[i].id) {
+				user = users[i];
+			}
+		}
+		user.x = data.x;
+		user.y = data.y;
+		socket.to(data.room).emit('receive-move', users)
+	})
+
 	socket.on('disconnect', () => {
+
 	})
 })
 
